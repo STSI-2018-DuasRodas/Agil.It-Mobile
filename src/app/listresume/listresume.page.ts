adc5fcb3ec692b4e5b938d56ef6739c6e1c2e8af
Julio <julio_thomazelli-ju1@estudante.sc.senai.br>
2020-09-01 18:49:14 -0300

    Ajuste Operation na lista

--- src/app/listresume/listresume.page.ts
+++ src/app/listresume/listresume.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EventEmitterService } from '../eventemitter/eventemitter.service';
-import { CadOperationComponent } from '../cad-operation/cad-operation.component';
+import { CadOperationComponent, CadOperationTypes } from '../cad-operation/cad-operation.component';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -33,18 +33,24 @@ export class ListResumePage implements OnInit, OnDestroy {
     item.operation_expanded = !item.operation_expanded;
   }
 
-  expandComponent(item){
-    this.presentModal(item);
-  }
-
-  async presentModal(data) {
+  async expandComponent(operation, orderEquipment){
     const modal = await this.modalController.create({
       component: CadOperationComponent,
       componentProps: {
-        'operationData' : data
+        'operationData' : operation,
+        'orderEquipID'  : orderEquipment.id,
+        'operationMoviment': CadOperationTypes.EDIT
       }
     });
     return await modal.present();
+  }
+
+  addOperation(){
+    
+  }
+
+  async presentModal(data) {
+    
   }
 
 }
